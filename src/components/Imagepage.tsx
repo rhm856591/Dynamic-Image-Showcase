f5481fcf664f2c4e211f35918822e0932d65431b
Rahamtullah sheikh <76489436+rhm856591@users.noreply.github.com>
2024-12-12 18:06:12 +0530

    just

--- src/components/Imagepage.tsx
+++ src/components/Imagepage.tsx
@@ -3,20 +3,24 @@ import Image from "next/image";
 export default function ImagePage() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="flex flex-col md:flex-row w-full md:w-4/5 h-3/5 overflow-hidden rounded-3xl relative">
-        <div className="relative flex-1 md:flex-1 transition-all duration-500 ease-in-out hover:md:flex-[4] group">
+      <div className="flex w-full md:w-4/5 h-3/5 overflow-hidden rounded-3xl relative flex-col md:flex-row">
+        
+        {/* Left Image */}
+        <div className="relative flex-1 transition-all duration-500 ease-in-out group md:hover:flex-[4] flex-[2] md:flex-1 group-hover:flex-[4]">
           <Image
             src="/gym-1.jpg"
             alt="Image 1"
             layout="fill"
             objectFit="cover"
-            className="rounded-t-lg md:rounded-l-lg md:rounded-tr-none"
+            className="rounded-t-lg md:rounded-l-lg"
           />
           <div className="absolute top-4 left-4 text-white text-lg font-bold bg-opacity-60 px-3 py-1 rounded">
             STRENGTH
           </div>
         </div>
-        <div className="relative flex-[2.5] md:flex-[2.5] transition-all duration-500 ease-in-out hover:md:flex-[3] group">
+
+        {/* Center Image */}
+        <div className="relative transition-all duration-500 ease-in-out group md:flex-[2.5] md:hover:flex-[3] flex-[2] group-hover:flex-[2.5]">
           <Image
             src="/gym-3.jpg"
             alt="Image 2"
@@ -27,13 +31,15 @@ export default function ImagePage() {
             MOBILITY
           </div>
         </div>
-        <div className="relative flex-1 md:flex-1 transition-all duration-500 ease-in-out hover:md:flex-[4] group">
+
+        {/* Right Image */}
+        <div className="relative flex-1 transition-all duration-500 ease-in-out group md:hover:flex-[4] flex-[2] md:flex-1 group-hover:flex-[4]">
           <Image
             src="/gym-2.png"
             alt="Image 3"
             layout="fill"
             objectFit="cover"
-            className="rounded-b-lg md:rounded-r-lg md:rounded-bl-none"
+            className="rounded-b-lg md:rounded-r-lg"
           />
           <div className="absolute top-4 left-4 text-white text-lg font-bold bg-opacity-60 px-3 py-1 rounded">
             DRILLS
